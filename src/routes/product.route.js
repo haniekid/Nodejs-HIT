4ecd4b1434a4679a63c59ee2f4d91cd403aaac74
thu ha <thuharosy2003@gmai.com>
2023-04-04 20:14:29 +0700

    create base

--- src/routes/product.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const productController = require("../controllers/product.controller");
-
-router.get("/", productController.viewProducts);
-router.get("/add", productController.addProducts);
-
-module.exports = router;
