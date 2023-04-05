95974877962c77b58a2c18e9a41e31ce09e623f9
thu ha <thuharosy2003@gmai.com>
2023-04-05 11:13:09 +0700

    loading

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,56 +1,60 @@
 const data = require("../data/dataProducts");
 const Product = require("./../models/product.model");
-const getProducts = (req, res) => {
-  // const products = require("../data/dataProducts");
 
-  // res.render("products", {
-  //   products,
-  //   title: "SẢN PHẨM",
-  // });
-  console.log(req.query);
+// [GET] / /products
+
+// ?key=value
+const getProducts = (req, res) => {
   let products = data;
   if (req.query.price) {
     products = products.filter((item) => item.price == req.query.price);
   }
-
   res.json(products);
-
-  // res.render("products", {
-  //   title: "My Product",
-  //   check: false,
-  //   products: products,
+  // Product.find({}, function (err, products) {
+  //   if (!err) {
+  //     res.json(products);
+  //   } else {
+  //     res.status(404).json({ error: "ERROR!!!" });
+  //   }
   // });
+  // res.render("products");
 };
 
+// [GET] / /products/id
 const getProductbyId = (req, res) => {
-  let products = data;
-  // console.log(req.params.productId);
-  products = products.filter((product) => product.id == req.params.productId);
-  res.json(products);
-  // res.render("products", {
-  //   title: "My Product",
-  //   check: false,
-  //   products: products,
-  // });
+  // let products = data;
+  // products = products.filter((product) => product.id == req.params.productId);
+  // res.json(products);
+
+  Product.findById(id, function (err, products) {
+    if (!err) {
+      res.json(products);
+    } else {
+      res.status(404).json({ error: "ERROR!!!" });
+    }
+  });
+  res.render("products");
 };
 
+// [GET] / /products/create
 const createProducts = (req, res) => {
   res.render("add-product", {
     title: "Thêm sản phẩm",
   });
 };
 
+// [POST] / /products/post
 const postProducts = (req, res) => {
   const product = req.body;
   res.json(product);
-  // console.log(product);
 };
 
+// [PUT] / /products/update
 const updateProductById = (req, res) => {};
 
+// [DELETE] / /products/delete
 const deleteProductById = (req, res) => {};
 
-// ?key=value
 module.exports = {
   getProducts,
   createProducts,
