a4a61d0225a46dcf7b46dfdaaf0a7d0d2df73492
thu ha <thuharosy2003@gmai.com>
2023-04-05 12:40:51 +0700

    practice with views and controllers

--- nodejs/f8/src/util/mongoose.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    multipleMongooseToObject: (mongooseArray) => {
-        return mongooseArray.map(mongooseElement =>
-            mongooseElement.toObject(),
-        );
-    },
-    mongooseToObject: (mongoose) => mongoose.toObject(),
-};
