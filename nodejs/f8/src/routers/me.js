615b5bdf8422c3a9e1c154ae963e43118605d6a4
thu ha <thuharosy2003@gmai.com>
2023-04-05 11:16:42 +0700

    lesson_04

--- nodejs/f8/src/routers/me.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const meRouter = express.Router()
-
-const meController = require('../app/controllers/MeController')
-
-meRouter.get('/stored/courses', meController.storedCourses)
-meRouter.get('/trash/courses', meController.trashCourses)
-
-module.exports = meRouter
